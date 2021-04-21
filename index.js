95595fe8343428c83962edc282bd653ae7de09fb
MiguelED <migueled2695@gmail.com>
2021-04-21 01:59:38 -0500

    npm i request

--- index.js
+++ index.js
@@ -1,11 +1,22 @@
 console.log( 'Starting' );
-
+const valor =0;
 setTimeout(
     () => console.log( '2 Second Timer' )
 , 2000 );
 
 setTimeout(
-    () => console.log( '0 Second Timer' )
+    () => {console.log( '3 Second Timer' ); this.valor =2;}
+, 3000 );
+
+setTimeout(
+    () => {
+        console.log( '0 Second Timer');
+        setTimeout(()=> console.log('4 Second timer'),1500);
+} 
 , 0 );
 
-console.log( 'Stopping' );
+console.log( 'Stopping ' + valor );
+
+setTimeout(
+    () => console.log( '1 Second Timer' )
+, 1000 );
